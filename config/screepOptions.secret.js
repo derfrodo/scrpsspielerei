ff5b82a8a1f4a7b1a3099a116430abe5bcc58b5b
derfrodo <derfrodo0715@gmail.com>
2017-09-14 15:51:43 +0200

    no more secrets

--- config/screepOptions.secret.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    branch: '$activeWorld',
-    email: 'DerFrodo',
-    password: '123456',
-    serverUrl: 'http://127.0.0.1:21025',
-    serverPassword: '',
-    gzip: false
-};
